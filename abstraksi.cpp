696669526577ca6bd5122160f02f7b37f5182986
sheilloputratama <sheilloggendut@gmail.com>
2025-06-02 18:23:55 +0700

    menambahkan method setXY

--- abstraksi.cpp
+++ abstraksi.cpp
@@ -6,3 +6,11 @@ class AbstraksiKlas
 private:
     string x, y;
 
+public:
+    //method untuk mengisi nilai
+    //private member
+    void setXY(string a, string b)
+    {
+        x = a;
+        y = b;
+    }
