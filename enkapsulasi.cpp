0027769851785e9b4b864e73e0148f93d4ac395f
sheilloputratama <sheilloggendut@gmail.com>
2025-06-02 18:57:50 +0700

    menambahkan fungsi main

--- enkapsulasi.cpp
+++ enkapsulasi.cpp
@@ -16,3 +16,19 @@ public:
         return saklarNo[i];
     }
 };
+
+int main()
+{
+    remoteLampu lampuRumah;
+    lampuRumah.setSaklarNo(0, "Lampu Teras Rumah");
+    lampuRumah.setSaklarNo(1, "Lampu Teras Tamu");
+    lampuRumah.setSaklarNo(2, "Lampu Teras Tidur");
+    lampuRumah.setSaklarNo(3, "Lampu Dapur");
+
+    cout << lampuRumah.getSaklarNo(0) << endl;
+    cout << lampuRumah.getSaklarNo(1) << endl;
+    cout << lampuRumah.getSaklarNo(2) << endl;
+    cout << lampuRumah.getSaklarNo(3) << endl;
+
+    return 0;
+}
